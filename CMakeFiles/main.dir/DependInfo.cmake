b6ce8acd6e8d5ce129f3e977f2487d316a6b721d
RausConn <cora4829@colorado.edu>
2023-02-18 22:58:14 -0700

    update readme.md

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cora4829/PokeBackend/code/Move.cpp" "/home/cora4829/PokeBackend/CMakeFiles/main.dir/code/Move.cpp.o"
+  "/home/cora4829/PokeBackend/code/MoveLibrary.cpp" "/home/cora4829/PokeBackend/CMakeFiles/main.dir/code/MoveLibrary.cpp.o"
+  "/home/cora4829/PokeBackend/code/Player.cpp" "/home/cora4829/PokeBackend/CMakeFiles/main.dir/code/Player.cpp.o"
+  "/home/cora4829/PokeBackend/code/Pokemon.cpp" "/home/cora4829/PokeBackend/CMakeFiles/main.dir/code/Pokemon.cpp.o"
+  "/home/cora4829/PokeBackend/code/PokemonLibrary.cpp" "/home/cora4829/PokeBackend/CMakeFiles/main.dir/code/PokemonLibrary.cpp.o"
+  "/home/cora4829/PokeBackend/code/Type.cpp" "/home/cora4829/PokeBackend/CMakeFiles/main.dir/code/Type.cpp.o"
+  "/home/cora4829/PokeBackend/code/main.cpp" "/home/cora4829/PokeBackend/CMakeFiles/main.dir/code/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "code"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
